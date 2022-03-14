adcb7fb3bc8f239c84a7e40b9f9b9ce5dc4f9878
Matthew Johnson <33819487+Matthew-Johnson833@users.noreply.github.com>
2022-03-14 16:49:02 -0700

    Consolidated btns. Data is stored in localStorage

--- volley_stats_app/src/servingButtonComponents/ServingAttemptBtn.js
+++ volley_stats_app/src/servingButtonComponents/ServingAttemptBtn.js
@@ -7,6 +7,7 @@ const ServingButtons = () => {
     servingAttempts: 0,
     servingAce: 0,
     servingError: 0,
+    servingInPercentage: 0,
   });
 
   function setServingAttempt() {
@@ -28,12 +29,22 @@ const ServingButtons = () => {
   function setServingError() {
     setServingArray(prevState => ({
       ...prevState, 
-      hittingAttempts: (servingArray.hittingAttempts + 1),
+      servingAttempts: (servingArray.servingAttempts + 1),
       servingError: (servingArray.servingError + 1)
     }))
     console.log("Service Errors: " + servingArray.servingError);
   }
   function generateServingPercentage() {
+    // React state doesn't immediately update. Need to click twice to get proper values
+    let calcServingPercentage = (((servingArray.servingAttempts - servingArray.servingError) / servingArray.servingAttempts).toFixed(2));
+    setServingArray(prevState => ({
+      ...prevState,
+      servingInPercentage: calcServingPercentage
+    }))
+    localStorage.setItem('servingArray', JSON.stringify(servingArray));
+
+
+    console.log(servingArray);
     console.log("Service In Percentage: "+ (((servingArray.servingAttempts - servingArray.servingError) / servingArray.servingAttempts)* 100).toFixed(0)+ "%");
   }
 
