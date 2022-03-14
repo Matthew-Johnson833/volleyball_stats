d700ef85f28d94232657d59467f066d5ae035233
Matthew Johnson <33819487+Matthew-Johnson833@users.noreply.github.com>
2022-03-14 16:47:53 -0700

    Consolidated btns. Data is stored in localStorage

--- volley_stats_app/src/components/hittingButtonComponents/HittingAttemptBtn.js
+++ volley_stats_app/src/components/hittingButtonComponents/HittingAttemptBtn.js
@@ -1,44 +1,58 @@
 import React, { useState } from "react";
 
 
-// function incrementHittingAttempt() {
-//      StatLine.setPlayerStats([... StatLine.playerStats, {
-//        hittingAttempts: StatLine.hittingAttempts + 1,
-//      }])
-//   console.log('Hitting Attempt');
-//   console.log(StatLine.playerStats.hittingAttempts)
-// }
-
-
 // RENAME THIS FILE. ALL BUTTONS FOR A SINGLE STAT LINE WILL BE IN ONE COMPONENT
-const HittingAttemptBtn = () => {
+const HittingButtons = () => {
 
   const [hittingArray, setHittingArray] = useState({
-    hittingAttempts: 1,
-    hittingKill: 1,
-    hittingError: 1,
-  });
+    // array values. CLG(0) = 1
+    hittingAttempts: 0,
+    hittingKill: 0,
+    hittingError: 0,
+    hittingPercentage: 0,
+    // create a function to generate the hitting % 
+    // call that function and store the value here.
+    });
+
   function setHittingAttempt() {
     setHittingArray(prevState => ({
       ...prevState, 
       hittingAttempts: (hittingArray.hittingAttempts + 1)
     }))
-    // Doesn't throw an error but instead logs 'undefined'???
     console.log("Hitting Attempts: " + hittingArray.hittingAttempts);
   }
   function setHittingKill() {
     setHittingArray(prevState => ({
       ...prevState, 
+      hittingAttempts: (hittingArray.hittingAttempts + 1),
       hittingKill: (hittingArray.hittingKill + 1)
     }))
+    console.log("Hitting Attempts: " + hittingArray.hittingAttempts);
     console.log("Hitting Kills: " + hittingArray.hittingKill);
   }
   function setHittingError() {
     setHittingArray(prevState => ({
       ...prevState, 
+      hittingAttempts: (hittingArray.hittingAttempts + 1),
       hittingError: (hittingArray.hittingError + 1)
     }))
     console.log("Hitting Errors: " + hittingArray.hittingError);
+  }
+  function generateHittingPercentage() {
+    // React state doesn't immediately update. Need to click twice to get proper values
+    let calcHittingPercentage = (((hittingArray.hittingKill - hittingArray.hittingError)
+                              / hittingArray.hittingAttempts)).toFixed(3);
+    setHittingArray(prevState => ({
+      ...prevState,
+      hittingPercentage: calcHittingPercentage
+    }))
+    console.log(hittingArray);
+    console.log("Hitting Percentage: "+ calcHittingPercentage);                                   
+    localStorage.setItem('hittingArray', JSON.stringify(hittingArray));
+    localStorage.setItem('hittingArray', JSON.stringify(hittingArray));
+    console.log(hittingArray);
+
+    // next --> grab and parse localStorage in the desired location.
   }
 
   return (
@@ -52,9 +66,12 @@ const HittingAttemptBtn = () => {
       <button onClick={setHittingError}>
         ERR
       </button>
+      <button onClick={generateHittingPercentage}>
+        HIT%
+      </button>
     </div>
 
   )
 }
 
-export default HittingAttemptBtn
+export default HittingButtons
